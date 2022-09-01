d687d0c456096874e82fa445226af9e52723a4b4
Fred Lindsey <fred.lindsey.92@gmail.com>
2022-09-01 16:48:13 -0500

    first database creation, table import, and local server instance

---
+++ gtd_import.sql
@@ -0,0 +1,11 @@
+USE GTD;
+
+# This will create our empty table
+SELECT * FROM gtd_no_nulls;
+					
+
+
+# use a wizard to import data all at once
+# data cleaning with SQL: cast data, str_to_date
+
+						
